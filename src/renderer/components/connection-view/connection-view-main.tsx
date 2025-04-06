c0023c944849ce1bbca76ac34476590ee1b853f6
BrentMRPalmer <bpalm015@uottawa.ca>
2025-04-06 18:32:30 -0400

    chat css (part 2)

--- src/renderer/components/connection-view/connection-view-main.tsx
+++ src/renderer/components/connection-view/connection-view-main.tsx
@@ -61,7 +61,7 @@ function ConnectionViewMain() {
         <TabsContent
           key={id}
           value={id}
-          className='overflow-hidden data-[state=inactive]:hidden'
+          className='flex-grow overflow-hidden data-[state=inactive]:hidden'
           forceMount
         >
           {component}
